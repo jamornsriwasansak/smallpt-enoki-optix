76d2691acc09b294d2fbeed2acaaca97ba6c3991
jamorn.sriwasansak <zentojamorn@gmail.com>
2019-12-27 22:52:52 +0900

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "enoki/cuda.h"
+#include "enoki/array.h"
+#include "enoki/autodiff.h"
+#include "enoki/dynamic.h"
+#include "enoki/stl.h"
+
+template <typename Value> Value srgb_gamma(Value x) {
+	return enoki::select(
+		x <= 0.0031308f,
+		x * 12.92f,
+		enoki::pow(x * 1.055f, 1.f / 2.4f) - 0.055f
+	);
+}
+
+int main()
+{
+	using FloatC = enoki::CUDAArray<float>;
+	using FloatD = enoki::DiffArray<FloatC>;
+	using Color3fD = enoki::Array<FloatD, 3>;
+
+	Color3fD input = Color3fD(0.5f, 1.0f, 2.0f);
+	enoki::set_requires_gradient(input);
+
+	Color3fD output = srgb_gamma(input);
+
+	FloatD loss = enoki::norm(output - Color3fD(.1f, .2f, .3f));
+	enoki::backward(loss);
+
+	std::cout << enoki::gradient(input) << std::endl;
+	return 0;
+}
