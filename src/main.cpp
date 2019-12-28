41e9ec1bad1486ed77aa966918993013a7c9f551
jamorn.s <jamorn.sriwasansak@gmail.com>
2019-12-29 03:12:50 +0900

    optix prime and cuda are added, added tinyobjloader

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #define NOMINMAX
-#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <vector>
 #include <algorithm>
 
 #include <iostream>
@@ -9,8 +11,44 @@
 #include "enoki/dynamic.h"
 #include "enoki/cuda.h"
 #include "enoki/stl.h"
+
 #include <optix.h>
-#include <optixu/optixpp.h>
+#include <optix_prime/optix_prime.h>
+#include <optix_prime/optix_prime_declarations.h>
+#include <optix_prime/optix_primepp.h>
+#include <cuda_runtime.h>
+
+struct Ray
+{
+	static const RTPbufferformat format = RTP_BUFFER_FORMAT_RAY_ORIGIN_TMIN_DIRECTION_TMAX;
+
+	float3 origin;
+	float  tmin;
+	float3 dir;
+	float  tmax;
+};
+
+struct Hit
+{
+	static const RTPbufferformat format = RTP_BUFFER_FORMAT_HIT_T_TRIID_U_V;
+
+	float t;
+	int   triId;
+	float u;
+	float v;
+};
+
+struct HitInstancing
+{
+	static const RTPbufferformat format = RTP_BUFFER_FORMAT_HIT_T_TRIID_INSTID_U_V;
+
+	float t;
+	int   triId;
+	int   instId;
+	float u;
+	float v;
+};
+
 
 template <typename Value> Value srgb_gamma(Value x) {
 	return enoki::select(
@@ -22,7 +60,48 @@ template <typename Value> Value srgb_gamma(Value x) {
 
 int main()
 {
-	optix::Context optixContext = optix::Context::create();
+#if 1
+	RTPcontexttype context_type = RTP_CONTEXT_TYPE_CUDA;
+	RTPbuffertype buffer_type = RTP_BUFFER_TYPE_CUDA_LINEAR;
+#else
+	RTPcontexttype contextType = RTP_CONTEXT_TYPE_CPU;
+	RTPbuffertype bufferType = RTP_BUFFER_TYPE_HOST;
+#endif
+
+	try
+	{
+		optix::prime::Context context = optix::prime::Context::create(context_type);
+		unsigned int device = 0;
+		context->setCudaDeviceNumbers(1, &device);
+
+		int num_triangles = 0;
+		int num_vertices = 0;
+		int3 *triangles;
+		float3 * vertices;
+
+		// model
+		optix::prime::Model model = context->createModel();
+		model->setTriangles(num_triangles, RTP_BUFFER_TYPE_HOST, triangles,
+							num_vertices,  RTP_BUFFER_TYPE_HOST, vertices);
+		model->update(RTPqueryhint::RTP_QUERY_HINT_NONE);
+
+		// query
+		optix::prime::Query query = model->createQuery(RTP_QUERY_TYPE_CLOSEST);
+		// TODO:: initialize hits and rays
+		Hit * hits;
+		Ray * rays;
+		cudaMalloc(&rays, sizeof(Ray) * 100);
+		cudaMalloc(&hits, sizeof(Hit) * 100);
+		optix::prime::BufferDesc buffer_desc;
+		query->setRays(100, Ray::format, buffer_type, rays);
+		query->setHits(100, Hit::format, buffer_type, hits);
+		query->execute(RTPqueryhint::RTP_QUERY_HINT_NONE);
+
+	}
+	catch (const std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 
 	using FloatC = enoki::CUDAArray<float>;
 	using FloatD = enoki::DiffArray<FloatC>;
